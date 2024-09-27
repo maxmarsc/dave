dc2240629f8ea42e21197ba736cf87346dda1d6f
maxmarsc <maxime.coutant@grenoble-inp.org>
2024-09-27 17:10:37 +0200

    Trying something

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
         "display": ["matplotlib>=2.2.3"],
         "doc": ["sphinx", "sphinx_rtd_theme", "numpydoc"],
     },
-    packages=find_packages(include=["dave", "dave/assets", "dave/debuggers", "dave/languages"]),
+    packages=find_packages(include=["dave", "dave.assets", "dave.debuggers", "dave.languages"]),
 )
