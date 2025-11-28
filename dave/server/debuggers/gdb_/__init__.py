d97ef95509f37485c42be5168530a26c741373d4
maxmarsc <maxime.coutant@grenoble-inp.org>
2025-11-28 22:00:55 +0100

    Added a debug hook server side to connect debugpy on port 5678

--- dave/server/debuggers/gdb_/__init__.py
+++ dave/server/debuggers/gdb_/__init__.py
@@ -18,3 +18,15 @@ gdb.events.stop.connect(stop_handler)
 gdb.pretty_printers.append(dave_printer)
 
 gdb.write("INFO : [dave] Successfully loaded\n")
+
+# If the flag is set, wait for the python debugger to connect
+if os.environ.get("DAVE_DEBUG_SERVER") == "1":
+    try:
+        import debugpy
+
+        gdb.write("Waiting for debugger to attach on port 5678...")
+        debugpy.listen(("localhost", 5678))
+        debugpy.wait_for_client()
+        gdb.write("Debugger attached! Continuing...")
+    except ImportError:
+        gdb.write("Error: 'debugpy' not found. Cannot attach debugger.")
--- dave/server/debuggers/lldb_/__init__.py
+++ dave/server/debuggers/lldb_/__init__.py
@@ -14,3 +14,15 @@ import dave.common.server_type as st
 st.SERVER_TYPE = st.ServerType.LLDB
 
 from ...languages import *
+
+# If the flag is set, wait for the python debugger to connect
+if os.environ.get("DAVE_DEBUG_SERVER") == "1":
+    try:
+        import debugpy
+
+        print("Waiting for debugger to attach on port 5678...")
+        debugpy.listen(("localhost", 5678))
+        debugpy.wait_for_client()
+        print("Debugger attached! Continuing...")
+    except ImportError:
+        print("Error: 'debugpy' not found. Cannot attach debugger.")
