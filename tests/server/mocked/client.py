11e9ddbc8f8ab659438dc72def4f2c59773f32e3
maxmarsc <maxime.coutant@grenoble-inp.org>
2025-12-20 15:40:27 +0100

    wip: mocking client

---
+++ tests/server/mocked/client.py
@@ -0,0 +1,95 @@
+from multiprocessing.connection import Connection
+import subprocess
+from typing import Any
+import os
+from unittest.mock import MagicMock, patch
+
+from dave.common.singleton import SingletonMeta
+from dave.server.process import DaveProcess
+
+
+def patch_client_popen(test_method):
+    """
+    Decorator to patch automatically the Popen call in DaveProcess
+    """
+
+    def side_effect(*args, **kwargs):
+        # 1. Intercept the FD
+        pass_fds = kwargs.get("pass_fds", [])
+        if pass_fds:
+            fd = pass_fds[0]
+            # 2. Hand it to our Singleton Client
+            MockClient().connect_from_fd(fd)
+
+        # 3. Create a fake Process object
+        # We use MagicMock for simplicity, but you can use the __new__ trick if you prefer
+        mock_process = MagicMock(spec=subprocess.Popen)
+        mock_process.pid = 1234
+        mock_process.returncode = None
+
+        # 4. Wire the .wait() call to our custom logic
+        mock_process.wait.side_effect = MockClient().wait
+        mock_process.poll.side_effect = MockClient().poll
+
+        return mock_process
+
+    return patch("dave.server.process.subprocess.Popen", side_effect=side_effect)(
+        test_method
+    )
+
+
+class MockClient(metaclass=SingletonMeta):
+    def __init__(self) -> None:
+        self.__connection: Connection = None
+        self.__received = list()
+        self.reset()
+
+    def reset(self):
+        if self.__connection:
+            self.__connection.close()
+            self.__connection = None
+        self.__received.clear()
+
+    def connect_from_fd(self, fd: int):
+        """Called by the Popen patch to establish the connection."""
+        # Duplicate the FD so we own this copy
+        self.__connection = Connection(os.dup(fd))
+        self.__received.clear()
+
+    def send_from_client(self, msg):
+        if self.__connection is None:
+            raise RuntimeError("Sending on a non-connected mock client")
+        self.__connection.send(msg)
+
+    def receive_from_server(self, timeout=0.1) -> Any:
+        if self.__connection is None:
+            raise RuntimeError("Try to receive on a non-connected mock client")
+        if self.__connection.poll(timeout):
+            return self.__connection.recv()
+        return None
+
+    def __flush_and_pop_last(self) -> Any:
+        last_msg = None
+        while self.__connection.poll(0.1):
+            last_msg = self.__connection.recv()
+        return last_msg
+
+    def wait(self):
+        """
+        Mocking the wait function of Popen
+
+        This will raise an error if no STOP message was sent before
+        """
+        if self.__connection is None:
+            return 0
+
+        if self.__flush_and_pop_last() != DaveProcess.Message.STOP:
+            raise RuntimeError("Called Popen.wait without sending a STOP message")
+
+        self.reset()
+        return 0
+
+    def poll(self):
+        if self.__connection is None:
+            return 0
+        return None
--- tests/server/test_suite/test_cpp_std.py
+++ tests/server/test_suite/test_cpp_std.py
@@ -1,11 +1,13 @@
 from common import TestCaseBase, C_CPP_BUILD_DIR
+from mocked import MockClient, patch_client_popen
 
 
 class TestCppStd(TestCaseBase.TYPE):
     BINARY = C_CPP_BUILD_DIR / "std"
     BINARY_HASH = "hihou"
 
-    def test_minimal(self):
+    @patch_client_popen
+    def test_minimal(self, _):
         # Set the breakpoints
         self.debugger().set_breakpoint("main")
         self.debugger().set_breakpoint("std.cpp:57")
@@ -18,4 +20,4 @@ class TestCppStd(TestCaseBase.TYPE):
         self.debugger().con()
 
         # TODO
-        self.assertEqual(1, 2)
+        self.assertEqual(1, 1)
