0f207287afac46f70776f9c3d511279ac23fbba5
maxmarsc <maxime.coutant@grenoble-inp.org>
2025-12-23 12:26:44 +0100

    Unified test output between gdb and lldb

--- tests/server/test_suite/run_tests.py
+++ tests/server/test_suite/run_tests.py
@@ -6,9 +6,21 @@ from pathlib import Path
 TEST_DIR = Path(__file__).parent
 
 
+class CustomTextTestResult(TextTestResult):
+    def startTest(self, test):
+        super(TextTestResult, self).startTest(test)
+        if self.showAll:
+            self.stream.write(self.getDescription(test))
+            self.stream.write(" ... \n")
+            self.stream.flush()
+            self._newline = True
+
+
 def run() -> TextTestResult:
     loader = TestLoader()
     test_suite = loader.discover(start_dir=TEST_DIR)
 
-    runner = TextTestRunner(verbosity=2, stream=sys.stdout)
+    runner = TextTestRunner(
+        verbosity=2, stream=sys.stdout, resultclass=CustomTextTestResult
+    )
     return runner.run(test_suite)
