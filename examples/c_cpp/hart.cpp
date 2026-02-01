d4e410599663430c8c372ced75fbfdd74b20f90b
maxmarsc <maxime.coutant@grenoble-inp.org>
2026-02-01 14:38:06 +0100

    Added tests for Hart library

--- examples/c_cpp/hart.cpp
+++ examples/c_cpp/hart.cpp
@@ -1,52 +1,52 @@
 #define HART_IMPLEMENTATION
 #include "hart.hpp"
 
+// NOLINTNEXTLINE(google-build-using-namespace)
 HART_DECLARE_ALIASES_FOR_FLOAT;
 using AudioBuffer = hart::AudioBuffer<float>;
 using hart::roundToSizeT;
 
-int main()
-{
-    constexpr double sampleRateHz = 44100.0;
+int main() {
+  constexpr double kSampleRateHz = 44100.0;
 
-    // Sine sweep
-    const size_t sineSweepDurationFrames = roundToSizeT (sampleRateHz * 1.0_s);
-    AudioBuffer bufferA (1, sineSweepDurationFrames);
-    auto sineSweepSignalA = SineSweep();
-    sineSweepSignalA.prepare (
-        sampleRateHz,
-        1,  // numOutputChannels
-        sineSweepDurationFrames  // maxBlockSizeFrames
-        );
-    sineSweepSignalA.renderNextBlock (bufferA);
+  // Sine sweep
+  const size_t sine_sweep_duration_frames = roundToSizeT(kSampleRateHz * 1.0_s);
+  AudioBuffer buffer_a(1, sine_sweep_duration_frames);
+  auto sine_sweep_signal_a = SineSweep();
+  sine_sweep_signal_a.prepare(kSampleRateHz,
+                              1,                          // numOutputChannels
+                              sine_sweep_duration_frames  // maxBlockSizeFrames
+  );
+  sine_sweep_signal_a.renderNextBlock(buffer_a);
 
-    // A different sweep, overwrites the same buffer
-    auto sineSweepSignalB = SineSweep()
-        .withType (SineSweep::SweepType::linear)
-        .withStartFrequency (100_Hz)
-        .withEndFrequency (1_Hz)
-        .withDuration (500_ms)
-        .withLoop (SineSweep::Loop::yes);
-    sineSweepSignalB.prepare (
-        sampleRateHz,
-        1,  // numOutputChannels
-        sineSweepDurationFrames  // maxBlockSizeFrames
-        );
-    sineSweepSignalB.renderNextBlock (bufferA);
+  // A different sweep, overwrites the same buffer
+  auto sine_sweep_signal_b = SineSweep()
+                                 .withType(SineSweep::SweepType::linear)
+                                 .withStartFrequency(100_Hz)
+                                 .withEndFrequency(1_Hz)
+                                 .withDuration(500_ms)
+                                 .withLoop(SineSweep::Loop::yes);
+  sine_sweep_signal_b.prepare(kSampleRateHz,
+                              1,                          // numOutputChannels
+                              sine_sweep_duration_frames  // maxBlockSizeFrames
+  );
+  sine_sweep_signal_b.renderNextBlock(buffer_a);
 
-    // Multi-channel noise
-    constexpr size_t multiChannelNoiseNumChannels = 5;
-    const size_t multiChannelNoiseDurationFrames = hart::roundToSizeT (sampleRateHz * 10_ms);
-    AudioBuffer bufferB (multiChannelNoiseNumChannels, multiChannelNoiseDurationFrames);
-    auto multiChannelNoiseSignal = WhiteNoise();
-    multiChannelNoiseSignal.prepare (
-        sampleRateHz,
-        multiChannelNoiseNumChannels,  // numOutputChannels
-        multiChannelNoiseDurationFrames  // maxBlockSizeFrames
-        );
-    multiChannelNoiseSignal.renderNextBlock (bufferB);
-    multiChannelNoiseSignal.renderNextBlock (bufferB);  // Some more noise...
-    multiChannelNoiseSignal.renderNextBlock (bufferB);  // ...and more noise
+  // Multi-channel noise
+  constexpr size_t kMultiChannelNoiseNumChannels = 5;
+  const size_t multi_channel_noise_duration_frames =
+      hart::roundToSizeT(kSampleRateHz * 10_ms);
+  AudioBuffer buffer_b(kMultiChannelNoiseNumChannels,
+                       multi_channel_noise_duration_frames);
+  auto multi_channel_noise_signal = WhiteNoise();
+  multi_channel_noise_signal.prepare(
+      kSampleRateHz,
+      kMultiChannelNoiseNumChannels,       // numOutputChannels
+      multi_channel_noise_duration_frames  // maxBlockSizeFrames
+  );
+  multi_channel_noise_signal.renderNextBlock(buffer_b);
+  multi_channel_noise_signal.renderNextBlock(buffer_b);  // Some more noise...
+  multi_channel_noise_signal.renderNextBlock(buffer_b);  // ...and more noise
 
-    return 0;
+  return 0;
 }
---
+++ tests/server/test_suite/test_cpp_hart.py
@@ -0,0 +1,96 @@
+from common import TestCaseBase, C_CPP_BUILD_DIR
+from dave.common.raw_iir import RawIir
+from mocked import MockClient, patch_client_popen
+
+from dave.common.raw_entity import RawEntity, RawEntityList, RawEntityUpdates
+from dave.common.raw_container import RawContainer
+from dave.common.sample_type import SampleType
+
+CONTAINER_2D_LAYOUTS = [
+    RawContainer.Layout.CPX_2D,
+    RawContainer.Layout.REAL_2D,
+]
+
+
+class TestCppHart(TestCaseBase.TYPE):
+    BINARY = C_CPP_BUILD_DIR / "hart_tests"
+    BINARY_HASH = "hihou"
+
+    def assertHartContainerInvariants(self, array: RawContainer):
+        self.assertListEqual(array.possible_layout, CONTAINER_2D_LAYOUTS)
+        self.assertEqual(array.dimensions_fixed, True)
+        self.assertEqual(array.interleaved, False)
+
+    @patch_client_popen
+    def test_audio_buffer(self, _):
+        # Set the breakpoints
+        self.debugger().set_breakpoints_at_tags("audioBuffer", [1, 2])
+
+        ################## audioBufferMultiChannel::1 - All zeros ##################
+        self.debugger().run()
+        with self.failFastSubTestAtLocation():
+            self.debugger().execute("dave show buffer_f")
+            self.debugger().execute("dave show buffer_d")
+
+            received = MockClient().receive_from_server()
+            self.assertIsListOf(received, 2, RawEntityList)
+
+            # buffer_f
+            self.assertEqual(len(received[0].raw_entities), 1)
+            self.assertIsInstance(received[0].raw_entities[0], RawContainer)
+            raw_buffer_f: RawContainer = received[0].raw_entities[0]
+            self.assertHartContainerInvariants(raw_buffer_f)
+            self.assertTupleEqual(raw_buffer_f.original_shape, (2, 3))
+            self.assertEqual(raw_buffer_f.sample_type, SampleType.FLOAT)
+            self.assertEqual(raw_buffer_f.default_layout, RawContainer.Layout.REAL_2D)
+            self.assertContainerContent((0.0, 0.0, 0.0, 0.0, 0.0, 0.0), raw_buffer_f)
+            self.assertPrettyPrinterEqual(
+                "buffer_f",
+                "2 channels 3 samples, min 0.0000E+00, max 0.0000E+00",
+                [("dSparkline[0]", '"[0(3)]"'), ("dSparkline[1]", '"[0(3)]"')],
+            )
+
+            # buffer_d
+            self.assertEqual(len(received[1].raw_entities), 1)
+            self.assertIsInstance(received[1].raw_entities[0], RawContainer)
+            raw_buffer_d: RawContainer = received[1].raw_entities[0]
+            self.assertHartContainerInvariants(raw_buffer_d)
+            self.assertTupleEqual(raw_buffer_d.original_shape, (2, 3))
+            self.assertEqual(raw_buffer_d.sample_type, SampleType.DOUBLE)
+            self.assertEqual(raw_buffer_d.default_layout, RawContainer.Layout.REAL_2D)
+            self.assertContainerContent((0.0, 0.0, 0.0, 0.0, 0.0, 0.0), raw_buffer_d)
+            self.assertPrettyPrinterEqual(
+                "buffer_d",
+                "2 channels 3 samples, min 0.0000E+00, max 0.0000E+00",
+                [("dSparkline[0]", '"[0(3)]"'), ("dSparkline[1]", '"[0(3)]"')],
+            )
+
+        ############ audioBufferMultiChannel::2 - (0,0,0),(1.0,-1.0,0.0) ############
+        self.debugger().continue_()
+        with self.failFastSubTestAtLocation():
+            received = MockClient().receive_from_server()
+            self.assertIsListOf(received, 2, RawContainer.InScopeUpdate)
+
+            # buffer_f
+            self.assertEqual(received[0].id, raw_buffer_f.id)
+            self.assertTupleEqual(received[0].shape, raw_buffer_f.original_shape)
+            self.assertContainerContent(
+                (0.0, 0.0, 0.0, 1.0, -1.0, 0.0), raw_buffer_f, received[0]
+            )
+            self.assertPrettyPrinterEqual(
+                "buffer_f",
+                "2 channels 3 samples, min -1.0000E+00, max 1.0000E+00",
+                [("dSparkline[0]", '"[0(3)]"'), ("dSparkline[1]", '"[‾x0]"')],
+            )
+
+            # buffer_d
+            self.assertEqual(received[1].id, raw_buffer_d.id)
+            self.assertTupleEqual(received[1].shape, raw_buffer_d.original_shape)
+            self.assertContainerContent(
+                (0.0, 0.0, 0.0, 1.0, -1.0, 0.0), raw_buffer_d, received[1]
+            )
+            self.assertPrettyPrinterEqual(
+                "buffer_d",
+                "2 channels 3 samples, min -1.0000E+00, max 1.0000E+00",
+                [("dSparkline[0]", '"[0(3)]"'), ("dSparkline[1]", '"[‾x0]"')],
+            )
